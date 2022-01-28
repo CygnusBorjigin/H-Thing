0c81a2fbb3d849b3fe0f09287534e182d2641796
SomeSomewhere <cygnusborjigin@gmail.com>
2022-01-27 20:27:37 -0500

    change List database model

--- backend_v2/models/List.js
+++ backend_v2/models/List.js
@@ -2,35 +2,56 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ListSchema = new mongoose.Schema({
-    user: {
-        type: Schema.Types.ObjectId,
-        ref: "users"
-    },
-    title: {
-        type: String,
-        required: true
-    },
-    items: [String],
-    status: {
-        finished: Boolean,
-        default: false
-    },
-    createdDate: {
-        type: Date,
-        default: Date.now
-    },
-    deadline: {
-        type: Date,
-        required: false
-    },
-    tag: {
-        type: [String],
-        required: false
-    },
-    description: {
-        type: String,
-        required: false
-    }
+	user: {
+		type: Schema.Types.ObjectId,
+		ref: "users"
+	},
+	title: {
+		type: String,
+		required: true
+	},
+	items: [{
+		content: {
+			type: String,
+			required: true
+		},
+		startDate: {
+			type: Date,
+			required: false
+		}
+		endDate: {
+			type: Date,
+			required: false
+		},
+		tag: {
+			type: [String],
+			required: false
+		}
+	}],
+	status: {
+		finished: Boolean,
+		default: false
+	},
+	createdDate: {
+		type: Date,
+		default: Date.now
+	},
+	startDate: {
+		type: Date,
+		required: false
+	},
+	deadline: {
+		type: Date,
+		required: false
+	},
+	tag: {
+		type: [String],
+		required: false
+	},
+	description: {
+		type: String,
+		required: false
+	}
 });
 
 module.exports = mongoose.model('list', ListSchema);
