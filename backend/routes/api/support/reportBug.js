9f43cee4e6783e522711af08a38505b77e59116e
SomeoneSomewhere <cygnusborjigin@gmail.com>
2022-05-20 11:12:17 -0400

    backend
    finish report bug route

--- backend/routes/api/support/reportBug.js
+++ backend/routes/api/support/reportBug.js
@@ -8,13 +8,33 @@ const cors = require('cors');
 report.use(cors());
 
 // import database schema
-const User = require('../../../models/User');
-const reportBug = require('../../../models/BugReport');
-const { model } = require('mongoose');
+const ReportBug = require('../../../models/BugReport');
 
 // handel request
 report.get('/', (req, res) => {
     res.send('This is the support / report route');
+});
+
+report.post('/bug', 
+            auth,
+            check('bug_detail', 'There is nothing in the report'),
+            async (req, res) => {
+                // check for error in the input
+                const errors = validationResult(req);
+                if (!errors.isEmpty()) return res.status(400).json({ message : errors.array()});
+                try {
+                    const newBugReport = new ReportBug({
+                        user: req.user.id,
+                        content: req.body.content
+                    });
+                    await newBugReport.save();
+                    res.status(200).json({ message: "reported" })
+                } catch (err) {
+                    console.log(err);
+                        res.status(500).json({
+                            errors: "server error",
+                        });
+                }
 });
 
 module.exports = report;
